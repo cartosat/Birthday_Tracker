456d125d77e49a971a93dad362140edf7a8bb92c
Vaibhav Wagaskar <cartosat4@gmail.com>
2022-11-20 01:32:16 +0530

    initilized Base app

---
+++ BirthDayTracker/defaultData.py
@@ -0,0 +1,77 @@
+import datetime
+from uuid import UUID
+
+# We are not using this anywhere. I have migrated all this data to DefaultFriend table in DB.
+class DataLoad:
+    DefaultDataPayLoad =  [
+        
+        {
+            "first_name": "Vaibhav",
+            "last_name": "Wagaskar",
+            "birth_date": datetime.date(1998, 5, 1),
+            "phone_number": "+917447359256",
+            "city": "Pune, india",
+            "email": "vaibhavwagaskar4@gmail.com",
+            "social_instagram": "vsw_04",
+            "social_facebook": "vaibhavwagaskar",
+            "social_twitter": "vsw_04",
+            "profile_image": "profiles/default-images/vaibhav-profile.png",
+            "id": "default-e70d-42d7-af4c-vaibhav"
+        },
+
+        {
+            "first_name": "Swaraj",
+            "last_name": "Gore",
+            "birth_date": datetime.date(1999, 3, 25),
+            "phone_number": "+919284905131",
+            "city": "Pune, india",
+            "email": "swarajgore97@gmail.com",
+            "social_instagram": "swaraj_gore_",
+            "social_facebook": "swarajgore",
+            "social_twitter": "swaraj_gore_",
+            "profile_image": "profiles/default-images/swaraj-profile.jpg",
+            "id": "default-e70d-42d7-af4c-swaraj"
+        },
+
+        {
+            "first_name": "Sakshi",
+            "last_name": "Gupta",
+            "birth_date": datetime.date(1999, 3, 16),
+            "phone_number": "+917775047786",
+            "city": "Bhopal, India",
+            "email": "sakangupt@gmail.com",
+            "social_instagram": "sakksshii",
+            "social_facebook": "sakshigupta",
+            "social_twitter": "sakksshii",
+            "profile_image": "profiles/default-images/sakshi-profile.jpeg",
+            "id": "default-e70d-42d7-af4c-sakshi"
+        },
+
+        {
+            "first_name": "Suyash",
+            "last_name": "Lokhande",
+            "birth_date": datetime.date(2000, 8, 14),
+            "phone_number": "+917775047786",
+            "city": "Jalgaon, India",
+            "email": "suyaslo@gmail.com",
+            "social_instagram": "suyash_0814",
+            "social_facebook": "suyashlokh",
+            "social_twitter": "suyash_0814",
+            "profile_image": "profiles/default-images/suyash-profile.jpg",
+            "id": "default-e70d-42d7-af4c-suyash"
+        },
+
+        {
+            "first_name": "Vidhi",
+            "last_name": "Khandelwal",
+            "birth_date": datetime.date(1998, 9, 5),
+            "phone_number": "+919511675768",
+            "city": "Jabalpur, India",
+            "email": "rvidhikh@gmail.com",
+            "social_instagram": "vidhi_05",
+            "social_facebook": "vidhik",
+            "social_twitter": "vidhi_05",
+            "profile_image": "profiles/default-images/vidhi-profile.jpg",
+            "id": "default-e70d-42d7-af4c-vidhi"
+        }
+    ]
